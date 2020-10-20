f944e488ed4532a5fc1b2af070cf87c505e13a18
mackeyonline <71295361+mackeyonline@users.noreply.github.com>
2020-10-19 20:37:00 -0500

    added functions to main.js

--- javascript_toolbox_inclass/main.js
+++ javascript_toolbox_inclass/main.js
@@ -1,9 +1,43 @@
-let myCoolBox = document.getElementById("box");
+$(document).ready(function(){
 
-myCoolBox.addEventListener("click", function(){
+    let fontSize = 16;
+    $('#box').on("click", function(){
+        console.log("i was clicked!, but in jquery");
+        printHello();
+        $('body').css('background', 'purple');
+        $('#box').css('font-size', `${fontSize}px` );
+        fontSize++;
+        $('#box').css('font-family', "Comic Sans MS");
+        $('#box').css('color', 'red');
+    })
 
 
 
-console.log("i was clicked");
+
+    // call the function
+    addDeezOn(5, 10);
+
+    // defining a function (addDeezOn aka addTheNumbers)
+    function addDeezOn(num1, num2) {
+        let sum = num1 + num2;
+        console.log(sum);
+    }
+
+    function printHello(){
+        console.log("hello");
+    }
+
 
 })
+
+
+
+// let myCoolBox = document.getElementById("box");
+
+// myCoolBox.addEventListener("click", function(){
+
+
+
+// console.log("i was clicked");
+
+// })
